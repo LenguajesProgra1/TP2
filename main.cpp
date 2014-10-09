de236a95a492f8e9614ec020ea634a2deb9aee8b
marianaRamirez <marianaRamirez@users.noreply.github.com>
2014-10-08 18:06:57 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,16 @@ using namespace std;
 
 int main ()
 {
+    // bienvenida al usuario en consola
     cout<< "----------Bienvenido al interprete de Prolog----------\n *Si desea crear un hecho debe escribir <define> y para terminar debe escribir </define>.\n *Si desea realizar una consulta solamente ingresela.\n";
 
-    ifstream archivo("bc.txt");
+    ifstream archivo("bc.txt");//busca el archivo txt y lo lee
     char linea[12];
     char *consul= "perro(gala).";
     if (archivo.good())
     {
         archivo >> linea;
-        if (strcmp(consul,linea)== 0)
+        if (strcmp(consul,linea)== 0) //compara cada linea del archivo con lo ingresado por el usuario
         {
             cout<< "Funciona \n";
             cout<<linea;
