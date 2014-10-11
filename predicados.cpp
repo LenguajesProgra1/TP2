50131557b407576cae2ff9aeb0af986aa15c105a
jcarlos0305 <jcarlos0305@hotmail.com>
2014-10-10 19:36:47 -0600

    predicados.cpp

--- predicados.cpp
+++ predicados.cpp
@@ -1,29 +1,45 @@
-#include <stdio.h>
-#include <string.h>
-#include<iostream>
-#include<fstream>
-#include<ctype.h>
-#include"predicados.h"
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+#include "predicados.h"
 
-int validarPredicado()
+int main ()
 {
-	char inicio[] = "<define>";
-	char fin[] = "</define>";
-	char valido[100];
-	char predicado[100];
-	ofstream txt ("bc.txt",std::ofstream::out | std::ofstream::app);
-	if(txt.is_open())
-	{
-		cout << "?- " ;
-		cin.getline(valido,100);
-		if(strcmp (inicio,valido)==0)
+	system("clear");
+    // bienvenida al usuario en consola
+    cout<< "- - - - - - - - - Bienvenido al interprete de Prolog- - - - - - - - - \n *Si desea crear un hecho debe escribir <define>. \n *Para terminar el proceso de creacion se escribe </define>.\n *Si desea realizar una consulta solamente ingresela.\n *Para cerrar el interprete ingrese la palabra SALIR.\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n?-";
+
+    ifstream archivo("bc.txt");//busca el archivo txt y lo lee
+    char linea[12];
+    char consul[]= "perro(gala).";
+	char salir[] = "SALIR";
+	char token[64];
+  	char inicio[] = "<define>";
+
+	do{
+		cin.getline(token,64);
+		if(strcmp(token,salir)==0)
+			return 0;
+		else
 		{
-			cin.getline(predicado,100);
-			while (strcmp (fin,predicado)!=0)
-			{	
-				txt << predicado << endl;
-				cin.getline(predicado,100);
+			if(strcmp(token,inicio)==0)
+				validarPredicado();
+			else
+			{
+				if (archivo.good())
+    			{
+        			archivo >> linea;
+        			if (strcmp(consul,linea)== 0) //compara cada linea del archivo con lo ingresado por el usuario
+					{
+						cout<< "Funciona \n";
+						cout<<linea;
+						archivo.close();
+					}  
+    			}
+    			else
+        			cout<< "Error al abrir el archivo";
 			}
 		}
-	}
+	}while(true);
 }
