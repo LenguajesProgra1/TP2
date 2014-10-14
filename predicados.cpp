60962699bee077e539beb3d5d6d0855659f04b76
jcarlos0305 <jcarlos0305@hotmail.com>
2014-10-14 08:27:45 -0600

    Update predicados.cpp

--- predicados.cpp
+++ predicados.cpp
@@ -1,45 +1,27 @@
-#include <iostream>
-#include <fstream>
-#include <cstring>
+#include <stdio.h>
+#include <string.h>
+#include<iostream>
+#include<fstream>
+#include<ctype.h>
 #include <cstdlib>
-#include "predicados.h"
+#include"predicados.h"
 
-int main ()
+int validarPredicado()
 {
 	system("clear");
-    // bienvenida al usuario en consola
-    cout<< "- - - - - - - - - Bienvenido al interprete de Prolog- - - - - - - - - \n *Si desea crear un hecho debe escribir <define>. \n *Para terminar el proceso de creacion se escribe </define>.\n *Si desea realizar una consulta solamente ingresela.\n *Para cerrar el interprete ingrese la palabra SALIR.\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n?-";
-
-    ifstream archivo("bc.txt");//busca el archivo txt y lo lee
-    char linea[12];
-    char consul[]= "perro(gala).";
-	char salir[] = "SALIR";
-	char token[64];
-  	char inicio[] = "<define>";
-
-	do{
-		cin.getline(token,64);
-		if(strcmp(token,salir)==0)
-			return 0;
-		else
-		{
-			if(strcmp(token,inicio)==0)
-				validarPredicado();
-			else
-			{
-				if (archivo.good())
-    			{
-        			archivo >> linea;
-        			if (strcmp(consul,linea)== 0) //compara cada linea del archivo con lo ingresado por el usuario
-					{
-						cout<< "Funciona \n";
-						cout<<linea;
-						archivo.close();
-					}  
-    			}
-    			else
-        			cout<< "Error al abrir el archivo";
-			}
+	cout << "- - - - - - - - - Modo Declaracion de Hechos - - - - - - - - - -\n*Para salir del proceso de declaracion se escribe </define>.\n - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n";
+	char fin[] = "</define>";
+	char predicado[100];
+	ofstream txt ("bc.txt",std::ofstream::out | std::ofstream::app);
+	if(txt.is_open())
+	{
+		cin.getline(predicado,100);
+		while (strcmp (fin,predicado)!=0)
+		{	
+			txt << predicado << endl;
+			cin.getline(predicado,100);
 		}
-	}while(true);
+		system("clear");
+		cout<< "- - - - - - - - - Bienvenido al interprete de Prolog- - - - - - - - - \n *Si desea crear un hecho debe escribir <define>. \n *Para terminar el proceso de creacion se escribe </define>.\n *Si desea realizar una consulta solamente ingresela.\n *Para cerrar el interprete ingrese la palabra SALIR.\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n?-";
+	}
 }
