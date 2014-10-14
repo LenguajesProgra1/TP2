a2c98892fba8d923357b69d8b28a6eee3b39fed5
jcarlos0305 <jcarlos0305@hotmail.com>
2014-10-14 08:28:10 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
-#include <fstream>
-#include <cstring>
 #include <cstdlib>
+#include <cstring>
 #include "predicados.h"
 
 int main ()
 {
 	system("clear");
     // bienvenida al usuario en consola
-    cout<< "- - - - - - - - - Bienvenido al interprete de Prolog- - - - - - - - - \n *Si desea crear un hecho debe escribir <define>. \n *Para terminar el proceso de creacion se escribe </define>.\n *Si desea realizar una consulta solamente ingresela.\n *Para cerrar el interprete ingrese la palabra SALIR.\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n?-";
-
-    ifstream archivo("bc.txt");//busca el archivo txt y lo lee
-    char linea[12];
-    char consul[]= "perro(gala).";
+    cout<< "- - - - - - - - - Bienvenido al interprete de Prolog- - - - - - - - - \n * Si desea crear un hecho debe escribir <define>. \n * Para terminar el proceso de creacion se escribe </define>.\n * Si desea realizar una consulta solamente ingresela.\n * Para cerrar el interprete ingrese la palabra SALIR.\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n?-";
+    
 	char salir[] = "SALIR";
 	char token[64];
   	char inicio[] = "<define>";
@@ -20,25 +16,17 @@ int main ()
 	do{
 		cin.getline(token,64);
 		if(strcmp(token,salir)==0)
+		{
+			system("clear");
 			return 0;
+		}
 		else
 		{
 			if(strcmp(token,inicio)==0)
 				validarPredicado();
 			else
 			{
-				if (archivo.good())
-    			{
-        			archivo >> linea;
-        			if (strcmp(consul,linea)== 0) //compara cada linea del archivo con lo ingresado por el usuario
-					{
-						cout<< "Funciona \n";
-						cout<<linea;
-						archivo.close();
-					}  
-    			}
-    			else
-        			cout<< "Error al abrir el archivo";
+				consulta(token);
 			}
 		}
 	}while(true);
