0139b320bc1dbae377da424cf76107c0999dc191
jcarlos0305 <jcarlos0305@hotmail.com>
2014-10-14 08:28:31 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,101 @@
 #include <iostream>
-#include <cstdlib>
+#include <fstream>
 #include <cstring>
+#include <cstdlib>
 #include "predicados.h"
 
-int main ()
-{
-	system("clear");
-    // bienvenida al usuario en consola
-    cout<< "- - - - - - - - - Bienvenido al interprete de Prolog- - - - - - - - - \n * Si desea crear un hecho debe escribir <define>. \n * Para terminar el proceso de creacion se escribe </define>.\n * Si desea realizar una consulta solamente ingresela.\n * Para cerrar el interprete ingrese la palabra SALIR.\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n?-";
-    
-	char salir[] = "SALIR";
-	char token[64];
-  	char inicio[] = "<define>";
+struct reglas {
+	char pred[16];
+	char args[32];
+	char ant[64];
+	};
 
-	do{
-		cin.getline(token,64);
-		if(strcmp(token,salir)==0)
+int buscar(char* antecedente)
+{	
+	char line[64];
+	ifstream bc("bc.txt");//busca el archivo txt y lo lee
+	while(Bc.good())
+	{
+		bc >> line;
+		if (strcmp(antecedente,line)== 0) //compara cada linea del archivo con lo ingresado por el usuario
 		{
-			system("clear");
-			return 0;
+			bc.close();
+			return 1;
 		}
-		else
+	else
+		return 0;
+	}
+}
+
+void consulta(char* consult)
+{
+	int bandera=0,x=0,y=0;
+	reglas consulta,regla;
+	memset(consulta.pred,0,16);
+	memset(consulta.args,0,32);
+	memset(consulta.ant,0,64);
+	memset(regla.pred,0,16);
+	memset(regla.args,0,32);
+	memset(regla.ant,0,64);
+	ifstream archivo("bc.txt");//busca el archivo txt y lo lee
+	char linea[64];
+	int encontrado = buscar(consult);
+	if (encontrado)
+	{
+		cout<<"YES"<<endl;
+		return;
+	}
+	while(bandera==0)
+	{
+		r.pred[y] = consult[x];
+		x++; y++;
+		if(consult[x] == 40)
 		{
-			if(strcmp(token,inicio)==0)
-				validarPredicado();
-			else
+			bandera++;
+			x++;
+		}
+	}
+	y=0;
+	while(bandera==1)
+	{
+		r.args[y] = consult[x];
+		x++; y++;
+		if(consult[x] == 41)
+		{
+			bandera++;
+			x++;
+		}
+	}
+	bandera=0;x=0;y=0;
+	while(archivo.good())
+	{
+		while(bandera==0)
+		{
+			regla.pred[y] = linea[x];
+			x++; y++;
+			if(linea[x] == 40)
 			{
-				consulta(token);
+				bandera++;
+				x++;
 			}
 		}
-	}while(true);
+	if(strcmp(consulta.pred = regla.pred)&&linea[x]>=65 && linea[x]<=90)
+	{
+		char temp[64]; int z=0;
+		bandera =0; y=0;
+		while(bandera==0)
+		{
+			temp[y] = consulta.p
+		}
+		
+	}
+		
+	}
+	
+	}
+	
+	
+	cout<<"FALSE"<<endl;
+	archivo.close();
+	return;
 }
